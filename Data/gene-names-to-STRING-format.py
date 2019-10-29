7aa8c410696a72fc93bf5d7513d2b1783721a566
oscardssmith <oscardssmith@gmail.com>
2019-10-29 11:31:50 -0500

    added lymphoma and endometriosis genes, script is better

--- Data/gene-names-to-STRING-format.py
+++ Data/gene-names-to-STRING-format.py
@@ -10,45 +10,52 @@ import requests
 import random
 
 # File with common gene names
-infile = 'IschaemicStrokeGenes.tsv'
+
+def read_genes(infile):
+   genes = []
+   with open(infile) as infile:
+       for line in infile:
+           # Don't read \n
+           genes.append(line[:-1])
+   return genes
 
 string_api_url = "http://string-db.org/api"
 output_format = "tsv-no-header"
 method = "get_string_ids"
 
-genes = []
-with open(infile) as ifile:
-    for line in ifile:
-        genes.append(line[:-1])
 ## contruct params dictionary
-
-params = {
+def gene_names_to_STRING(genes):
+    params = {
     "identifiers" : "\r".join(genes), # your protein list
     "species" : 9606, # species NCBI identifier 
     "limit" : 1, # only one (best) identifier per input protein
     "echo_query" : 1, # see your input identifiers in the output
     "caller_identity" : "www.awesome_app.org" # your app name
+    }
 
-}
+    ## contruct method URL
+    request_url = string_api_url + "/" + output_format + "/" + method
 
-## contruct method URL
+    ## Call STRING
+    try:
+        response = requests.post(request_url, params=params)
+    except requests.exceptions.RequestException as e:
+       print(e)
+       sys.exit()
+    return response
 
-request_url = string_api_url + "/" + output_format + "/" + method
+def output_response(response):
+    ## Read and parse the results
+    for line in response.text.strip().split("\n"):
+        fields = line.split("\t")
+        #print(fields)
+        input_identifier, string_identifier = fields[0], fields[2]
+        #print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
+        print(string_identifier)
 
-## Call STRING
-
-try:
-    response = requests.post(request_url, params=params)
-except requests.exceptions.RequestException as e:
-    print(e)
-    sys.exit()
-
-## Read and parse the results
-
-c = 0
-for line in response.text.strip().split("\n"):
-    l = line.split("\t")
-    #print(l)
-    input_identifier, string_identifier = l[0], l[2]
-    #print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
-    print(string_identifier)
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+       infile = sys.argv[1]
+    genes = read_genes(infile)
+    response = gene_names_to_STRING(genes)
+    output_response(response)
