6ce3ba02aec75263b6be2f423ea0833262a2ecbe
padillac <shinimaninima@gmail.com>
2019-11-05 15:19:19 -0600

    added load disease genes function to loader.py

--- RWR/RandomWalk.py
+++ RWR/RandomWalk.py
@@ -6,49 +6,33 @@ import loader
 import networkx as nx
 import matplotlib.pyplot as plt
 import time
+import numpy as np
 
 
-def load_graph(path):
-    """
-    Loads data from TSV file pointed to by path into a networkx graph
-    """
-    G = nx.Graph()
-    with open(path, 'r') as inputFile:
-        inputFile.readline()
-        for line in inputFile:
-            #if line.strip() == "#":
-            #    break
-            data = line.strip().split(" ")
-            #print("new edge:", data[0], data[1], data[2])
-            G.add_edge(data[0], data[1], confidence=data[2])
-    return G
-
-def main():
-    pathToData = "../test-graph-data.tsv"
-
-    #Read data from input file to networkx graph format.
-    startTime = time.time()
-    PPI_Graph = load_graph(pathToData)
-    endTime = time.time()
-
-    print("graph loaded from file.\nTime elapsed:", endTime - startTime, "seconds.")
 
 
-    #Visualize graph in matplotlib.
-    startTime = time.time()
-    nx.draw(PPI_Graph, node_color='r', edge_color='b')
-    endTime = time.time()
 
-    print("graph visualized.\nTime elapsed:", endTime - startTime, "seconds.")
-    plt.show()
+def randomWalkMatrix(matrix, start_vector, R, max_iterations, norm_threshold):
+
+    previous_vector = np.copy(start_vector)
+    iterations = 0
+    difference = float('inf')
+
+    while difference > norm_threshold and iterations < max_iterations:
+        #Perform one step of the walk
+        new_vector = (1 - R) * np.matmul(matrix, previous_vector)
+        new_vector = np.add(new_vector, R * start_vector)
+
+        difference = np.difference(new_vector, previous_vector)
+        previous_vector = new_vector
+        iterations += 1
+
+    return new_vector
 
 
-    #export to graphML file
-    startTime = time.time()
-    nx.write_graphml(PPI_Graph, "PPI_Network.graphml")
-    endTime = time.time()
 
-    print("graph exported.\nTime elapsed:", endTime - startTime, "seconds.")
+
+
 
 
 
@@ -63,7 +47,58 @@ def RandomWalk(graph, diseaseGeneList):
     @returns: a nested list of tuples, in sorted order of probability, where each item contains the name of a gene, and its respective probability as determined by the algorithm
     """
 
+
     return [['9606.ENSP00000351407', 0.45], ['genename4', 0.36], ['genename3', 0.301], ['genename2', 0.23]]
+
+
+
+
+
+def main():
+    pathToData = "../Data/test-graph-data.tsv"
+    pathToDiseaseGeneFile = "../Data/EndometriosisProteins.tsv"
+
+
+    #Read data from input file to networkx graph format.
+    startTime = time.time()
+    PPI_Graph = loader.load_graph(pathToData)
+    endTime = time.time()
+
+    print("Graph loaded from file.\nTime elapsed:", endTime - startTime, "seconds.")
+
+
+    #Read data from disease gene file into list
+    startTime = time.time()
+    diseaseGeneList = load_disease_genes(pathToDiseaseGeneFile)
+    endTime = time.time()
+
+    print("Disease genes loaded from file.\nTime elapsed:", endTime - startTime, "seconds.")
+
+
+    startTime = time.time()
+    probabilityVector = RandomWalk(PPI_Graph, diseaseGeneList)
+    endTime = time.time()
+
+    print("Random Walk matrix implementation finished running.\nTime elapsed:", endTime - startTime, "seconds.")
+    print(probabilityVector)
+
+
+    #Visualize graph in matplotlib.
+    #startTime = time.time()
+    #nx.draw(PPI_Graph, node_color='r', edge_color='b')
+    #endTime = time.time()
+
+    #print("graph visualized.\nTime elapsed:", endTime - startTime, "seconds.")
+    #plt.show()
+
+
+    #Export to graphML file
+    #startTime = time.time()
+    #nx.write_graphml(PPI_Graph, "PPI_Network.graphml")
+    #endTime = time.time()
+
+
+    print("graph exported.\nTime elapsed:", endTime - startTime, "seconds.")
 
 
 
--- Scripts/loader.py
+++ Scripts/loader.py
@@ -14,3 +14,15 @@ def load_graph(path):
             #print("new edge:", data[0], data[1], data[2])
             G.add_edge(data[0], data[1], confidence=data[2])
     return G
+
+
+
+def load_disease_genes(path):
+    """
+    Loads disease genes from TSV file and returns a python list of all names
+    """
+    with open(path, 'r') as inputFile:
+        list = []
+        for line in inputFile:
+            list.append(line)
+        return list
