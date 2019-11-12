a63e97cfc769ef9472fdbcf2c3142bcf25ac4349
chens5 <chens5@carleton.edu>
2019-11-11 21:55:30 -0600

    updated pagerank so it uses adjacency mat

--- PageRank/pagerank.py
+++ PageRank/pagerank.py
@@ -1,46 +1,40 @@
 import numpy as np
+from scipy.spatial import distance
 
-
-# Note, this is about 200x slower than np.linalg.norm(v1-v2)
-# https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.norm.html
-
-# Calculates the euclidean distance between two vectors
-def difference(v1, v2):
-    # v1 list of nodes
-    # v2 list of nodes
-    magnitude = 0
-    for i in range(len(v1)):
-        magnitude += (v1[i] - v2[i])**2
-    magnitude = np.sqrt(magnitude)
-    return magnitude
+EPSILON = 0.001
 
 # Given a np.array matrix, starting vector, prior bias vector, and back
 # probability, calculate the rank of each node in the graph.
-def pagerank(matrix, starting_vector, prior_bias, beta, epsilon):
-    # return all rankings
+def pagerank(adjacency_matrix, starting_vector, prior_bias, beta):
+    N = len(adjacency_matrix)
+    sqrt_d_inverse = np.zeros((N, N))
+    for i in range(N):
+        sqrt_d_inverse[i][i] = 1/np.sqrt(sum(adjacency_matrix[i]))
+    sqrt_d_inverse = np.array(sqrt_d_inverse)
+    matrix = np.matmul(np.matmul(sqrt_d_inverse, adjacency_matrix), sqrt_d_inverse)
+
     d = float('inf')
     prev_vector = np.copy(starting_vector)
     iterations = 0
-    # Probably should use squared distance as cheaper (no sqrt needed), and compare to epsilon*epsilon
-    # For squared distance, use scipy.spatial.distance.sqeuclidean. It is 1.5x faster than computing regular distance
-    while d > epsilon:
+    while d > EPSILON:
         result = (1 - beta) * np.matmul(matrix, prev_vector)
         result = np.add(result, beta*prior_bias)
-        d = difference(result, prev_vector)
+        d = distance.sqeuclidean(result, prev_vector)
         prev_vector = result
         iterations += 1
         print("iterations", iterations)
         print(result)
         print("difference", d)
+    # zip prev_vector and gene names
     return prev_vector
 
+
 def main():
-    matrix = np.array([[0, 1/2, 1/2, 1], [1/3, 0, 1/2, 0], [1/3, 1/2, 0, 0], [1/3, 0, 0, 0]])
+    matrix = np.array([[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 0], [1, 0, 0, 0]])
     starting_vector = np.array([1/4, 1/4, 1/4, 1/4])
     prior_bias = np.copy([0, 1/2, 0, 1/2])
     beta = 0.5
-    epsilon = 0.001
-    result = pagerank(matrix, starting_vector, prior_bias, beta, epsilon)
+    result = pagerank(matrix, starting_vector, prior_bias, beta)
     print(result)
 
 if __name__ == '__main__':
