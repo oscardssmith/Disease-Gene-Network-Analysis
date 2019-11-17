3b7e2ea31abe757772b8c815e6f05ad0d5201b89
Oscar Smith <oscardssmith@gmail.com>
2019-11-17 02:29:07 -0600

    now the thing I wrote works and has been tested

--- Scripts/GraphUtils.py
+++ Scripts/GraphUtils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 def normalize_adjacency_matrix(adjacency_matrix):
-    sqrt_d_inverse = np.diag(1/np.sqrt(np.sum(adjacency_matrix, axis=1)))
+    #ravel there so that diag is nx1 instead of 1xn
+    diag = np.ravel(1/np.sqrt(np.sum(adjacency_matrix, axis=1)))
+    sqrt_d_inverse = np.diag(diag)
     return np.matmul(np.matmul(sqrt_d_inverse, adjacency_matrix), sqrt_d_inverse)
