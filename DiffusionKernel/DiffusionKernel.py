ed38540a7f7d9d73e4c71dda34d288162f967af6
Oscar Smith <oscardssmith@gmail.com>
2019-11-20 00:27:28 -0600

    matrix is dense

--- DiffusionKernel/DiffusionKernel.py
+++ DiffusionKernel/DiffusionKernel.py
@@ -12,16 +12,18 @@ from scipy.linalg import expm
 #import ..RWR.RandomWalk import load_graph
 
 TEST_FILE = '../Data/fake.tsv'
-REAL_FILE = '../9606.protein.links.v11.0.tsv'
+REAL_FILE = '../Data/9606.protein.links.v11.0.txt'
 
 def diffusion_kernel(protein_graph, genes, B=1):
-    L = nx.laplacian_matrix(protein_graph)
+    print('in dk')
+    L = nx.laplacian_matrix(protein_graph).todense()
+    print('done densing')
     K = expm(-B * L)
-    return K * genes
+    return np.matmul(K,  genes)
 
 if __name__ == '__main__':
     t1 = time()
     #graph, label_to_idx = load_matrix(TEST_FILE, 0)
-    graph = load_graph(TEST_FILE)
+    graph = load_graph(REAL_FILE)
     print(time()-t1)
     #diffusion_kernel(graph, 1)
