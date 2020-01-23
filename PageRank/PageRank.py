a9f6905a0bda33b117d14825162825b1db912602
Andy <younkinsa@mirage>
2020-01-23 10:48:05 -0600

    cleaned up page rank

--- PageRank/PageRank.py
+++ PageRank/PageRank.py
@@ -1,42 +1,45 @@
 import sys
 sys.path.insert(1, '../Scripts/')
-from GraphUtils import normalize_adjacency_matrix, format_output
-import loader
-import networkx as nx
-import matplotlib.pyplot as plt
-import time
-import math
-import numpy as np
-from scipy.spatial import distance
-try:
-   import cPickle as pickle
-except:
-   import pickle
-
+import GraphUtils
 import os
+from scipy.spatial import distance
+import numpy as np
+import math
+import time
+import matplotlib.pyplot as plt
+import networkx as nx
+import loader
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 
 BETA = 0.3
+DISEASE_GENE_FILE_PATH = "../Data/EndometriosisProteins.tsv"
+DATA_PATH = "../Data/9606.protein.links.v11.0.txt"
 EPSILON = 10**(-6)
+PICKLE_PATH = "../Data/pickledmatrix"
 
 # Given a np.array matrix, starting vector, prior bias vector, and back
 # probability, calculate the rank of each node in the graph.
+
+
 def rank_genes(adjacency_matrix, starting_vector, prior_bias, beta):
     print("started ranking genes")
-    startTime = time.time()
+    start_time = time.time()
 
     # Load matrix from pickled object if exists to save time converting file.
-    if os.path.isfile("../Data/pickledmatrix"):
+    if os.path.isfile(PICKLE_PATH):
         print("pickled matrix file exists, loading matrix from file")
-        with open("../Data/pickledmatrix", 'rb') as handle:
+        with open(PICKLE_PATH, 'rb') as handle:
             matrix = np.asarray(pickle.load(handle))
     else:
-        matrix = np.asarray(normalize_adjacency_matrix(nx.to_numpy_matrix(graph)))
-        with open("../Data/pickledmatrix", 'wb') as handle:
+        matrix = np.asarray(GraphUtils.normalize_adjacency_matrix(
+            nx.to_numpy_matrix(graph)))
+        with open(PICKLE_PATH, 'wb') as handle:
             pickle.dump(matrix, handle)
-    endTime = time.time()
-    print("time elapsed for normalizing the adjacency matrix: ",endTime - startTime )
-
+    print("time elapsed for normalizing the adjacency matrix: ", time.time() - start_time)
 
     d = float('inf')
     prev_vector = np.copy(starting_vector)
@@ -49,6 +52,7 @@ def rank_genes(adjacency_matrix, starting_vector, prior_bias, beta):
         iterations += 1
         print("finished iterations", iterations)
     return prev_vector
+
 
 def load_priors(priors_file, graph):
     prior_bias = np.zeros(graph.number_of_nodes())
@@ -73,40 +77,36 @@ def load_priors(priors_file, graph):
     prior_bias = (1/total) * prior_bias
     return prior_bias
 
+
 def PageRank(graph, start_vector, prior_bias):
     adjacency_matrix = nx.to_numpy_matrix(graph)
-    return format_output(graph,rank_genes(adjacency_matrix, start_vector, prior_bias, BETA))
+    return GraphUtils.format_output(graph, rank_genes(adjacency_matrix, start_vector, prior_bias, BETA))
+
 
 def main():
-    pathToData = "../Data/9606.protein.links.v11.0.txt"
-    pathToDiseaseGeneFile = "../Data/EndometriosisProteins.tsv"
 
+    # Read data from input file to networkx graph format.
+    start_time = time.time()
+    PPI_Graph = loader.load_graph(DATA_PATH)
+    print("Graph loaded from file.\nTime elapsed:",
+          time.time() - start_time, "seconds.")
 
-    #Read data from input file to networkx graph format.
-    startTime = time.time()
-    PPI_Graph = loader.load_graph(pathToData)
-    endTime = time.time()
+    # Read data from disease gene file into list
+    start_time = time.time()
+    start_vector = loader.load_start_vector(DISEASE_GENE_FILE_PATH, PPI_Graph)
+    print("Disease genes loaded from file.\nTime elapsed:",
+          time.time() - start_time, "seconds.")
 
-    print("Graph loaded from file.\nTime elapsed:", endTime - startTime, "seconds.")
+    start_time = time.time()
+    probability_vector = PageRank(PPI_Graph, start_vector)
+    print("PageRank matrix implementation finished running.\nTime elapsed:",
+          time.time() - start_time, "seconds.")
 
-
-    #Read data from disease gene file into list
-    startTime = time.time()
-    start_vector = loader.load_start_vector(pathToDiseaseGeneFile, PPI_Graph)
-    endTime = time.time()
-
-    print("Disease genes loaded from file.\nTime elapsed:", endTime - startTime, "seconds.")
-
-
-    startTime = time.time()
-    probabilityVector = PageRank(PPI_Graph, start_vector)
-    endTime = time.time()
-
-    print("PageRank matrix implementation finished running.\nTime elapsed:", endTime - startTime, "seconds.")
-    print(probabilityVector)
-    sortedProbabilities = sorted(probabilityVector, key=lambda x: x[1])
+    print(probability_vector)
+    sortedProbabilities = sorted(probability_vector, key=lambda x: x[1])
     for i in range(50):
-        print("gene name: ", sortedProbabilities[i][0], "probability:", sortedProbabilities[i][1])
+        print("gene name: ", sortedProbabilities[i][0],
+              "probability:", sortedProbabilities[i][1])
 
 
 if __name__ == '__main__':
