bd51d5dc8b325732268b78d9c4a010627c0dba2e
chens5 <chens5@carleton.edu>
2020-01-26 17:05:05 -0600

    starting code for roc curve

--- Validation/areaUnderROC.py
+++ Validation/areaUnderROC.py
@@ -12,9 +12,32 @@ import DiffusionKernel as dk
 import PageRank as pr
 import loader
 import time
+import matplotlib as plt
+
+def roc_curve(result_vec, ground_truth_vec):
+    TPR = []
+    FPR = []
+    for threshhold in range(len(result_vec)):
+        tp = 0
+        fp = 0
+        fn = 0
+        tn = 0
+        for i in range(len(result_vec)):
+            item = result_vec[i]
+            if i < threshhold and item in ground_truth_vec:
+                tp += 1
+            elif i < threshhold:
+                fp += 1
+            elif item in ground_truth_vec:
+                fn += 1
+            else:
+                tn += 1
+        TPR.append(tp/(tp + fn))
+        FPR.append(fp/(fp + tn))
+    plt.pyplot.plot(FPR, TPR)
 
 def main():
-    # Get output vectors from each algorithm 
+    # Get output vectors from each algorithm
     priors_file_path= '../Data/LymphomaProteinsPriors.tsv'
     PPI_Network = load_PPI_Network('../Data/9606.protein.links.v11.0.txt') # load network
     startVector = loader.load_start_vector('../Data/LymphomaProteins.tsv', PPI_Network)
