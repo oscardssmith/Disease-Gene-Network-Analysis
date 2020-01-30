642a87f0f909903a9b0adc58c0733e9412aacbeb
padillac <shinimaninima@gmail.com>
2020-01-30 09:21:37 -0600

    changes to leaveoneout

--- Validation/leaveOneOut.py
+++ Validation/leaveOneOut.py
@@ -18,7 +18,8 @@ sys.path.insert(1, '../Scripts/')
 import RandomWalk as rwr
 import DiffusionKernel as dk
 import PageRank as pr
-import loader
+from CacheUtils import compute_if_not_cached
+from loader import load_graph, load_start_vector
 import time
 import numpy as np
 import networkx as nx
@@ -26,7 +27,7 @@ import networkx as nx
 
 
 
-def leaveOneOut(function, diseaseGeneFilePath, PPI_Network, param, priors_file_path=None):
+def leave_one_out(function, diseaseGeneFilePath, PPI_Network, param, priors_file_path=None):
     print("Starting leaveOneOut function")
 
     # building list of disease genes
@@ -93,42 +94,74 @@ def leaveOneOut(function, diseaseGeneFilePath, PPI_Network, param, priors_file_p
 
 
 
-
-def load_PPI_Network(filePath):
-    return loader.load_graph(filePath)
+# unnecessary?
+# def load_PPI_Network(filePath):
+#     return loader.load_graph(filePath)
 
 
 
 def main():
-    # # load the full PPI Network
-    totalStartTime = time.time()
-    print("Starting leave one out validation.")
-    print("Loading graph")
-    PPI_Network = load_PPI_Network('../Data/9606.protein.links.v11.0.txt')
-    print("Loaded graph")
-    file_paths = ['../Data/endometriosis-proteins.diseasegenes.tsv','../Data/lymphoma-proteins.diseasegenes.tsv', '../Data/ischaemic-stroke-proteins.diseasegenes.tsv']
-    prior_paths = ['../Data/endometriosis-proteins-priors.diseasegenes.tsv','../Data/lymphoma-proteins-priors.diseasegenes.tsv', '../Data/ischaemic-stroke-proteins-priors.diseasegenes.tsv']
-    rwr_pr_params = [0.2, 0.4, 0.6, 0.8]
-    dk_params = [0.4, 0.6, 0.8, 1.0]
 
-    for file_index in range(3):
-        for param_index in range(4):
-            print("--------------------DISEASE GENE FILE: ", file_paths[file_index], "PARAMETER: ", rwr_pr_params[param_index], "---------------------------------")
-            print("---------------RWR----------------------")
-            result_rwr = leaveOneOut(rwr.random_walk, file_paths[file_index], PPI_Network, rwr_pr_params[param_index])
-            print("percentage of genes improperly predicted for RWR:", result_rwr)
-            print("---------------PR----------------------")
-            result_pr = leaveOneOut(pr.page_rank, file_paths[file_index], PPI_Network, rwr_pr_params[param_index], prior_paths[file_index])
-            print("percentage of genes improperly predicted for PR:", result_pr)
-            print("---------------DK----------------------")
-            result_dk = leaveOneOut(dk.diffusion_kernel, file_paths[file_index], PPI_Network, dk_params[param_index], prior_paths[file_index])
-            print("percentage of genes improperly predicted for DK:", result_dk)
+    algorithm = sys.argv[1]
+    pathToPPINetworkFile = sys.argv[2]
+    pathToDiseaseGeneFile = sys.argv[3]
+    param = float(sys.argv[4])
 
-    # result = leaveOneOut(pr.PageRank, 'diseaseGeneFile', PPI_Network, priors_file_path)
-    # print("Mean squared difference for PageRank:", result)
+    print("loading data from files..")
+    ppiGraph = compute_if_not_cached(load_graph, pathToPPINetworkFile, fileName="ppiGraph")
+    
+    if algorithm == "Algorithms/DiffusionKernel.py":
+        function = dk.DiffusionKernel
+    if algorithm == "Algorithms/PageRank.py":
+        function = pr.PageRank
+    if algorithm == "Algorithms/RandomWalk.py":
+        function = rwr.RandomWalk
 
-    totalEndTime = time.time()
-    print("Finished leave one out validation!\nTotal time in hours:", (totalEndTime - totalStartTime)/3600)
+    leave_one_out(function, pathToDiseaseGeneFile, ppiGraph, param)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # # # load the full PPI Network
+    # totalStartTime = time.time()
+    # print("Starting leave one out validation.")
+    # print("Loading graph")
+    # PPI_Network = load_PPI_Network('../Data/9606.protein.links.v11.0.txt')
+    # print("Loaded graph")
+    # file_paths = ['../Data/endometriosis-proteins.diseasegenes.tsv','../Data/lymphoma-proteins.diseasegenes.tsv', '../Data/ischaemic-stroke-proteins.diseasegenes.tsv']
+    # prior_paths = ['../Data/endometriosis-proteins-priors.diseasegenes.tsv','../Data/lymphoma-proteins-priors.diseasegenes.tsv', '../Data/ischaemic-stroke-proteins-priors.diseasegenes.tsv']
+    # rwr_pr_params = [0.2, 0.4, 0.6, 0.8]
+    # dk_params = [0.4, 0.6, 0.8, 1.0]
+
+    # for file_index in range(3):
+    #     for param_index in range(4):
+    #         print("--------------------DISEASE GENE FILE: ", file_paths[file_index], "PARAMETER: ", rwr_pr_params[param_index], "---------------------------------")
+    #         print("---------------RWR----------------------")
+    #         result_rwr = leaveOneOut(rwr.random_walk, file_paths[file_index], PPI_Network, rwr_pr_params[param_index])
+    #         print("percentage of genes improperly predicted for RWR:", result_rwr)
+    #         print("---------------PR----------------------")
+    #         result_pr = leaveOneOut(pr.page_rank, file_paths[file_index], PPI_Network, rwr_pr_params[param_index], prior_paths[file_index])
+    #         print("percentage of genes improperly predicted for PR:", result_pr)
+    #         print("---------------DK----------------------")
+    #         result_dk = leaveOneOut(dk.diffusion_kernel, file_paths[file_index], PPI_Network, dk_params[param_index], prior_paths[file_index])
+    #         print("percentage of genes improperly predicted for DK:", result_dk)
+
+    # # result = leaveOneOut(pr.PageRank, 'diseaseGeneFile', PPI_Network, priors_file_path)
+    # # print("Mean squared difference for PageRank:", result)
+
+    # totalEndTime = time.time()
+    # print("Finished leave one out validation!\nTotal time in hours:", (totalEndTime - totalStartTime)/3600)
 
 
 
