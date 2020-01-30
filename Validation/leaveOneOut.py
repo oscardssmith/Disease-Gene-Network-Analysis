9bb858251b5bc8ea2a78ff0947a873eb3588e8d2
padillac <shinimaninima@gmail.com>
2020-01-30 08:10:49 -0600

    fixed algorithm paths in files

--- Validation/leaveOneOut.py
+++ Validation/leaveOneOut.py
@@ -12,9 +12,8 @@ Each algorithm produces results in a 2 dimensional array where each item is a tu
 
 """
 import sys
-sys.path.insert(1, '../RWR/')
-sys.path.insert(1, '../DiffusionKernel/')
-sys.path.insert(1, '../PageRank/')
+sys.path.insert(1, '../Algorithms/')
+sys.path.insert(1, 'Algorithms/')
 sys.path.insert(1, '../Scripts/')
 import RandomWalk as rwr
 import DiffusionKernel as dk
