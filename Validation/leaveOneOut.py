d1684f2c4082ec30e81d44d73f1a5f8ad8f5845e
padillac <shinimaninima@gmail.com>
2020-02-10 16:17:24 -0600

    removed priors files from eligible disease gene files

--- Validation/leaveOneOut.py
+++ Validation/leaveOneOut.py
@@ -32,7 +32,7 @@ def leave_one_out(function, diseaseGeneFilePath, PPI_Network, param, priors_file
 
     # building list of disease genes
     diseaseGeneFile = open(diseaseGeneFilePath, 'r')
-    allDiseaseGenes = diseaseGeneFile.read().splitlines().strip()
+    allDiseaseGenes = diseaseGeneFile.read().splitlines()
     diseaseGeneFile.close()
 
     numDiseaseGenes = len(allDiseaseGenes)
