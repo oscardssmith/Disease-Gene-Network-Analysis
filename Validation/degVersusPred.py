9ef98bb2389abf44106a1abd55ea2a0c3090e250
Kate Finstuen-magro <finstuenk@carleton.edu>
2020-02-16 19:20:38 -0600

    debugged degVersusPred.py

--- Validation/degVersusPred.py
+++ Validation/degVersusPred.py
@@ -9,17 +9,18 @@ def unpack_output(file):
 	with open(file, 'r') as input_file:
 		for line in input_file.readlines():
 			line = line.strip('\n').split('\t')
-			if int(line[2]) == -1:
-				predicted_out.append(line[1])
+			if int(int(line[2])) == -1:
+				predicted_out.append(int(line[1]))
 			else:
-				predicted_in.append(line[1])
+				predicted_in.append(int(line[1]))
 	return predicted_in, predicted_out
 
 def main():
 	# for each file
 		#pred_in, pred_out = unpack_output(file)
-	pred_in = [5, 4, 5, 6, 2, 3]
-	pred_out = [10, 11, 29, 2]
+	pred_in, pred_out = unpack_output("Validation/leave_one_out_endometriosis-proteins_rwr.tsv")
+	#pred_in = [5, 4, 5, 6, 2, 3]
+	#pred_out = [10, 11, 29, 2]
 	fig1, ax = plt.subplots()
 	ax.boxplot([pred_in, pred_out])
 	rand_x_in = []
